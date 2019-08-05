70dcffe4d4c32f1a5fbf4aa02c54dfd6132476e5
Csaba Kiss <csakis@lanl.gov>
2019-08-05 17:41:21 -0600

    added bulma

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import {Container} from 'react-bulma-components';
+
+
+function App() {
+  return ( 
+    <Container>
+
+      
+    </Container>
+    
+  );
+}
+
+export default App;
