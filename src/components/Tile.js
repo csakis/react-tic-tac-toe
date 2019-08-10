39bf0e71f22552fb01d3b01fadfa119928424e8a
Csaba Kiss <csakis@lanl.gov>
2019-08-10 10:50:27 -0600

    refactored Status and Tile to functional component

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,35 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card } from "react-bulma-components";
 import "./Tile.css";
 import "../../node_modules/@fortawesome/fontawesome-pro/css/all.css";
 
-class Tile extends Component {
-  state = {};
-  render() {
-    let icon = "far fa-4x ";
-    switch (this.props.tile) {
-      case -1:
-        icon += "fa-times has-text-success";
-        break;
-      case 0:
-        icon += "fa-blank fa-stop";
-        break;
-      case 1:
-        icon += "fa-circle has-text-danger";
-        break;
+const Tile = props => {
+  let icon = "far fa-4x ";
+  switch (props.tile) {
+    case -1:
+      icon += "fa-times has-text-success";
+      break;
+    case 0:
+      icon += "fa-blank fa-stop";
+      break;
+    case 1:
+      icon += "fa-circle has-text-danger";
+      break;
     default:
-        icon += "fa-blank fa-stop";
-        break;
-    }
-
-    return (
-      <Card onClick={this.props.setTile}>
-        <Card.Content className="has-text-centered">
-          <span className={icon} />
-        </Card.Content>
-      </Card>
-    );
+      icon += "fa-blank fa-stop";
+      break;
   }
-}
+
+  return (
+    <Card onClick={props.setTile}>
+      <Card.Content className="has-text-centered">
+        <span className={icon} />
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default Tile;
